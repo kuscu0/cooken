0e0a7811fb46e9aa71f1b59562b1557e67be9f7a
SeelenBacken <mail@soulback.de>
2020-10-29 12:44:52 +0100

    DataBaseTest

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var mongo = require('mongodb');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
