ae1046046f45e641dbd02fa947b953de8b95eeb6
Arthur Heitmann <art.tec.15@gmail.com>
2020-11-01 19:39:31 +0100

    made crawler multithreaded

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -3,9 +3,22 @@ import re
 import json
 import time
 import codecs
+import threading
 
 allIds = []
+pendingIds = []
+pendingIdsLock = threading.Lock()
+allIdsFound = False
+t_firstReq = 0
+t_apiReq = 0
+t_idSearch = 0
+t_fileWrite = 0
 
+startPage = 500
+endPage = 1000
+currentPage = 0
+threadsToUse = 4
+remainingIdsLoggingInterval = 15
 
 def getAllPrevIds():
 	global allIds
@@ -15,8 +28,6 @@ def getAllPrevIds():
 	except:
 		pass  # file doesn't exist, no problem, since allIds have already been initialized
 
-
-# print(allIds)
 
 
 def writeAllIds():
@@ -28,24 +39,30 @@ def writeAllIds():
 def pageUrl(pageNum):
 	return "https://www.chefkoch.de/rs/s%do3/Rezepte.html" % (pageNum * 30)
 
+def statusPrinter():
+	startTime = time.time()
+	while len(pendingIds) > 0 or not allIdsFound:
+		print("remaining ids: %d \t\t page crawler progress: %.2f \t\t seconds passed: %d"
+			% (len(pendingIds), ((currentPage - startPage) / (endPage - startPage)), time.time() - startTime))
+		time.sleep(remainingIdsLoggingInterval)
 
-def main():
-	getAllPrevIds()
-	t_total = time.time()
-	t_firstReq = 0
-	t_apiReq = 0
-	t_idSearch = 0
-	t_fileWrite = 0
+def idsCollector():
+	global currentPage
+	global allIdsFound
+	global pendingIds
+	global t_firstReq
+	global t_idSearch
 
-	for page in range(3):
-		print("page %d" % page)
+
+	for page in range(startPage, endPage):
+		currentPage = page
 		t1 = time.time()
 		firstRequest = requests.get(pageUrl(page))
 		html = firstRequest.text
 		t_firstReq += time.time() - t1
 		t1 = time.time()
 		jsonStart = html.find('<script type="application/ld+json">',
-							  html.find('<script type="application/ld+json">') + 1)
+							html.find('<script type="application/ld+json">') + 1)
 		jsonEnd = html.find("</script>", jsonStart)
 		entries = html[jsonStart:jsonEnd]
 		ids = re.findall("https://www\\.chefkoch\\.de/rezepte/(\\d+)/.*\\.html", entries)
@@ -55,22 +72,55 @@ def main():
 			if id in allIds:
 				continue
 			allIds.append(id)
+			pendingIdsLock.acquire()
+			pendingIds.append(id)
+			pendingIdsLock.release()
+	print("ids collector is done")
+	allIdsFound = True
 
-			t1 = time.time()
-			request = requests.get("https://api.chefkoch.de/v2/recipes/" + str(id))
-			t_apiReq += time.time() - t1
+def recipeDataDownloader():
+	global t_apiReq, t_fileWrite
 
-			t1 = time.time()
-			with codecs.open("recipes/%s.json" % id, "w+", "utf-8") as f:
-				f.write(json.dumps(request.json(), indent=4, ensure_ascii=False)
-						.encode("utf-8")
-						.decode())
-			t_fileWrite += time.time() - t1
-			# break
+	while len(pendingIds) > 0 or not allIdsFound:
+		pendingIdsLock.acquire()
+		if len(pendingIds) == 0:
+			if allIdsFound:
+				pendingIdsLock.release()
+				return
+			else:
+				pendingIdsLock.release()
+				continue
 
-		# break
+		id = pendingIds.pop()
+		pendingIdsLock.release()
 
-	# print(allIds)
+		t1 = time.time()
+		request = requests.get("https://api.chefkoch.de/v2/recipes/" + str(id))
+		t_apiReq += time.time() - t1
+
+		t1 = time.time()
+		with codecs.open("recipes/%s.json" % id, "w+", "utf-8") as f:
+			f.write(json.dumps(request.json(), indent=4, ensure_ascii=False)
+					.encode("utf-8")
+					.decode())
+		t_fileWrite += time.time() - t1
+
+def main():
+	getAllPrevIds()
+	t_total = time.time()
+
+	threading.Thread(target=statusPrinter).start()
+	idsCollectorThread = threading.Thread(target=idsCollector)
+	idsCollectorThread.start()
+	apiThreads = []
+	for i in range(threadsToUse):
+		apiThreads.append(threading.Thread(target=recipeDataDownloader))
+		apiThreads[i].start()
+
+	idsCollectorThread.join()
+	for thread in apiThreads:
+		thread.join()
+
 	t_total = time.time() - t_total
 
 	print("t_total: " + str(t_total))
@@ -78,6 +128,8 @@ def main():
 	print("t_apiReq: " + str(t_apiReq))
 	print("t_idSearch: " + str(t_idSearch))
 	print("t_fileWrite: " + str(t_fileWrite))
+
+	print("last page: " + str(currentPage))
 
 	writeAllIds()
 
