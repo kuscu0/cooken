2fa27c6e0000db6736cc15ef7fb6997a80b72591
SeelenBacken <mail@soulback.de>
2020-11-06 12:49:47 +0100

    Database Test on Local MongoDB successful

---
+++ server/tests/test_database_local.js
@@ -0,0 +1,18 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if(err) {
+        console.log(err);
+    }
+    const collection = client.db("cooken").collection("recipes");
+    // perform actions on the collection object
+    collection.insertOne({ name: "Spätzle", persons: "20000" }, err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Inserted");
+        }
+    })
+});
