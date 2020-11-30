b9671e95b001176aa235d04bcf0d66f8d25a5a48
kuscu0 <deniz@kuscu.de>
2020-11-30 17:02:23 +0100

    Dynamic Search Recipes, small improvements

--- src/pages/recipeSearch/RecipeSearch.jsx
+++ src/pages/recipeSearch/RecipeSearch.jsx
@@ -3,12 +3,33 @@ import InputText from "../../basics/inputText/InputText";
 import DropDown from "../../basics/dropDown/DropDown";
 import RecipeTile from "../../basics/recipeTile/RecipeTile";
 import SimpleButton from "../../basics/simpleButton/simpleButton";
+import React from "react";
 
 export default function RecipeSearch() {
+
+	const recipes = [];
+
+	for (let i = 0; i < 20; i++) {
+		recipes.push({ title: "Spaghetti Carbonara", img: "/img/carbonara.jpg", url: "/recipe" });
+	}
+
+	const [searchTerm, setSearchTerm] = React.useState("");
+	const [searchResults, setSearchResults] = React.useState([]);
+	const handleChange = event => {
+		setSearchTerm(event.target.value);
+	};
+
+	React.useEffect(() => {
+		const results = recipes.filter(recipe =>
+			recipe.title.toLowerCase().includes(searchTerm)
+		);
+		setSearchResults(results);
+	}, [searchTerm]);
+
 	return (
 		<div className="recipeSearch">
 			<div className="optionsBar">
-				<InputText placeholder="Search..."/>
+				<InputText placeholder="Search..." autoFocus value={searchTerm} onChange={handleChange}/>
 				<DropDown
 					expanderChildren={
 						<div>Must use</div>
@@ -24,11 +45,7 @@ export default function RecipeSearch() {
 			</div>
 			<div className="results">
 				{
-					Array(20).fill({
-						title: "Spaghetti Carbonara",
-						img: "/img/carbonara.jpg",
-						url: "/recipe"
-					}).map(recipe => (
+					searchResults.map(recipe => (
 						<RecipeTile recipeUrl={recipe.url} recipeTitle={recipe.title} recipeImg={recipe.img}/>
 					))
 				}
