50f8c3984fd13c394000e0af35f0c2d649bb0ffe
ArthurHeitmann <art.tec.15@gmail.com>
2020-12-04 19:41:11 +0100

    added script to make ingredients db from recipe db; + get grouped ingredients api endpoint

--- server/db/Connection.js
+++ server/db/Connection.js
@@ -3,77 +3,105 @@ const connectionErr = require("./connectionErr");
 const bcrypt = require("bcrypt");
 
 class Connection {
-    saltRounds = 10
+	saltRounds = 10
 
-    constructor(uri) {
-        this.client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-    }
+	constructor(uri) {
+		this.client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+	}
 
-    async hashPassword(password) {
-        return await bcrypt.hash(password, this.saltRounds);
-    }
+	async hashPassword(password) {
+		return await bcrypt.hash(password, this.saltRounds);
+	}
 
-    async checkPasswordValidity(password, hashedPassword) {
-        return await bcrypt.compare(password, hashedPassword);
-    }
+	async checkPasswordValidity(password, hashedPassword) {
+		return await bcrypt.compare(password, hashedPassword);
+	}
 
-    async createUser(usrName, usrPW, usrMail) {
-        if (usrName === "")
-            throw "User Name must not be empty";
-        if (usrMail === "")
-            throw "User E-Mail must not be empty";
-        const hashedPw = await this.hashPassword(usrPW);
-        const db = (await this.client.connect()).db("cooken");
-        const existingUsers = await db.collection("users").find({ email: usrMail });
-        if (await existingUsers.count())
-            throw "E-Mail unavailable";
-        await db.collection("users").insertOne({
-            name: usrName,
-            email: usrMail,
-            password: hashedPw,
-        });
-        return "success";
-    }
+	async createUser(usrName, usrPW, usrMail) {
+		if (usrName === "")
+			throw "User Name must not be empty";
+		if (usrMail === "")
+			throw "User E-Mail must not be empty";
+		const hashedPw = await this.hashPassword(usrPW);
+		const db = (await this.client.connect()).db("cooken");
+		const existingUsers = await db.collection("users").find({email: usrMail});
+		if (await existingUsers.count())
+			throw "E-Mail unavailable";
+		await db.collection("users").insertOne({
+			name: usrName,
+			email: usrMail,
+			password: hashedPw,
+		});
+		return "success";
+	}
 
-    async updateUser(usrName, usrPW, usrMail) {
-        const hashedPw = await this.hashPassword(usrPW);
-        const db = (await this.client.connect()).db("cooken");
-        await db.collection("users").updateOne(
-            {
-                email: usrMail
-            },
-            {
-                name: usrName,
-                email: usrMail,
-                password: hashedPw,
-            }
-        );
-        return "success";
-    }
+	async updateUser(usrName, usrPW, usrMail) {
+		const hashedPw = await this.hashPassword(usrPW);
+		const db = (await this.client.connect()).db("cooken");
+		await db.collection("users").updateOne(
+			{
+				email: usrMail
+			},
+			{
+				name: usrName,
+				email: usrMail,
+				password: hashedPw,
+			}
+		);
+		return "success";
+	}
 
-    async removeUser(usrMail) {
-        const db = (await this.client.connect()).db("cooken");
-        await db.collection("users").deleteOne(
-            {
-                email: usrMail
-            }
-        )
-        return "success";
-    }
+	async removeUser(usrMail) {
+		const db = (await this.client.connect()).db("cooken");
+		await db.collection("users").deleteOne(
+			{
+				email: usrMail
+			}
+		)
+		return "success";
+	}
 
-    async checkUser(usrMail, usrPW) {
-        const db = (await this.client.connect()).db("cooken");
-        const result = await db.collection("users").findOne(
-            {
-                email: usrMail
-            }
-        );
-        return await this.checkPasswordValidity(usrPW, result.password) ? result : null;
-    }
+	async checkUser(usrMail, usrPW) {
+		const db = (await this.client.connect()).db("cooken");
+		const result = await db.collection("users").findOne(
+			{
+				email: usrMail
+			}
+		);
+		return await this.checkPasswordValidity(usrPW, result.password) ? result : null;
+	}
 
-    async login() {
-
-    }
+	async getIngredientGroups() {
+		const db = (await this.client.connect()).db("cooken");
+		return new Promise((resolve, reject) => {
+			db.collection("ingredients").aggregate(
+				[
+					{
+						'$group': {
+							'_id': '$category',
+							'ingredients': {
+								'$push': '$name'
+							},
+							'count': {
+								'$sum': 1
+							}
+						}
+					},
+					{
+						'$sort': {
+							'count': -1
+						}
+					},
+					{
+						'$unset': "count"
+					}
+				],
+				async (err, result) => {
+					resolve(await result.toArray())
+				}
+			);
+		});
+	}
 }
 
 module.exports = Connection;
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,96 +2,87 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const jwt = require("jsonwebtoken");
-const { jwtSecret } = require("../secets")
+const {jwtSecret} = require("../secets")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
 });
 
 router.put('/', asyncHandler(async (req, res, next) => {
-    let DB = req.app.get('DB');
-    try {
-        res.send(await DB.createUser(req.body.name, req.body.password, req.body.email));
-    }
-    catch (e) {
-        res.status(e.statusCode || 500).json(e);
-        console.error(e);
-    }
+	let DB = req.app.get('DB');
+	try {
+		res.send(await DB.createUser(req.body.name, req.body.password, req.body.email));
+	} catch (e) {
+		res.status(e.statusCode || 500).json(e);
+		console.error(e);
+	}
 }));
 
 router.patch('/', asyncHandler(async (req, res, next) => {
-    let DB = req.app.get('DB');
-    try {
-        res.send(await DB.updateUser(req.body.name, req.body.password, req.body.email));
-    }
-    catch (e) {
-        res.status(e.statusCode || 500).json(e);
-        console.error(e);
-    }
+	let DB = req.app.get('DB');
+	try {
+		res.send(await DB.updateUser(req.body.name, req.body.password, req.body.email));
+	} catch (e) {
+		res.status(e.statusCode || 500).json(e);
+		console.error(e);
+	}
 }));
 
 router.delete('/', asyncHandler(async (req, res, next) => {
-    let DB = req.app.get('DB');
-    try {
-        res.send(await DB.removeUser(req.body.email));
+	let DB = req.app.get('DB');
+	try {
+		res.send(await DB.removeUser(req.body.email));
 
-    }
-    catch (e) {
-        res.status(e.statusCode || 500).json(e);
-        console.error(e);
-    }
+	} catch (e) {
+		res.status(e.statusCode || 500).json(e);
+		console.error(e);
+	}
 }));
 
-// router.post('/check', asyncHandler(async (req, res, next) => {
-//     let DB = req.app.get('DB');
-//     try {
-//         res.send(await DB.checkUser(req.body.email, req.body.password));
-//     }
-//     catch (e) {
-//         res.status(e.statusCode || 500).json(e);
-//         console.error(e);
-//     }
-// }));
-
 router.post("/login", asyncHandler(async (req, res, next) => {
-    let DB = req.app.get('DB');
-    const { email, password } = req.body;
-    const foundUser = await DB.checkUser(email, password);
-    if (foundUser) {
-        res.json({
-           token: jwt.sign(
-               { uid: foundUser._id },
-               jwtSecret,
-               {
-                   expiresIn: 60 * 60 * 24 * 7      // valid for 6 days
-               }
-           )
-        });
-    }
-    else {
-        res.status(401).send("Wrong credentials");
-    }
+	let DB = req.app.get('DB');
+	const {email, password} = req.body;
+	const foundUser = await DB.checkUser(email, password);
+	if (foundUser) {
+		res.json({
+			token: jwt.sign(
+				{uid: foundUser._id},
+				jwtSecret,
+				{
+					expiresIn: 60 * 60 * 24 * 7      // valid for 6 days
+				}
+			)
+		});
+	} else {
+		res.status(401).send("Wrong credentials");
+	}
 }));
 
 router.get("/savedRecipes", verifyJwt, asyncHandler(async (req, res, next) => {
-    res.send("Wow my recipes!");
+	res.send("Wow my recipes!");
+}));
+
+router.get("/ingredientGroups", asyncHandler(async (req, res, next) => {
+	let DB = req.app.get('DB');
+	res.json({ ingredientGroups: await DB.getIngredientGroups() });
 }));
 
 function verifyJwt(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        res.status(401).send("Missing authorization header");
-    }
-    const token = authHeader.match(/(?<=Bearer\s+)(.+)/i)[0];
-    jwt.verify(token, jwtSecret, (err, data) => {
-        if (err) {
-            res.status(403).send("invalid token");
-            return
-        }
-        req.uid = data.uid;
-        next();
-    });
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		res.status(401).send("Missing authorization header");
+	}
+	const token = authHeader.match(/(?<=Bearer\s+)(.+)/i)[0];
+	jwt.verify(token, jwtSecret, (err, data) => {
+		if (err) {
+			res.status(403).send("invalid token");
+			return
+		}
+		req.uid = data.uid;
+		next();
+	});
 }
+
 
 module.exports = router;
