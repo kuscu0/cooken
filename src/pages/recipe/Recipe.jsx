cac6186d9eee4795edbbac00b241de8043be75cd
ArthurHeitmann <art.tec.15@gmail.com>
2020-12-05 09:22:10 +0100

    added recipe page loading

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,56 +1,66 @@
 import "./Recipe.scss";
+import React, {useEffect, useState} from "react";
+import {serverAddress} from "../../globals";
+import {useLocation} from "react-router-dom";
 
 export default function Recipe() {
+	const [recipeData, setRecipeData] = useState(null);
+	const location = useLocation();
+
+	useEffect(() => {
+		fetch(`${serverAddress}/users${location.pathname}`).then(async response => {
+			if (response.status !== 200)
+				throw "Error getting recipe";
+			setRecipeData(await response.json());
+		})
+	}, [location.pathname]);
+
 	return (
 		<div className="recipePage paddedPage">
-			<h1>Spaghetti Carbonara</h1>
-			<img src="/img/carbonara.jpg" alt="carbonara" className="recipeImage"/>
+			<h1>{recipeData?.title}</h1>
+			<img src={`https://img.chefkoch-cdn.de/rezepte/${recipeData?._id}/bilder/${recipeData?.previewImageId}/crop-552x552/${recipeData?.title.replace(/\s/g, "-")}.jpg`} alt="carbonara" className="recipeImage"/>
 			<div className="ingredients">
 				{
-					[{
-						groupName: "Title",
-						ingredients: [
-							{ amount: 400, unit: "g", name: "Spaghetti" },
-							{ amount: 200, unit: "g", name: "Schinken", other: "roher" },
-							{ amount: 4, unit: "", name: "Eigelb"},
-							{ amount: 50, unit: "g", name: "Butter" },
-							{ amount: 0, unit: "", name: "Salz und Pfeffer" },
-							{ amount: 1, unit: "Prise(n)", name: "Muskat" },
-							{ amount: 0, unit: "n. B.", name: "Parmesan", other: "frisch gerieben" },
-						]
-					}].map(ingredientsGroup => (
-						<div className="ingredientGroup">
-							{ ingredientsGroup.groupName && <h3>{ingredientsGroup.groupName}:</h3>}
+					recipeData?.ingredientGroups.map((ingredientsGroup, i) => (
+						<div className="ingredientGroup" key={i}>
+							{!/\s*/.test(ingredientsGroup.header) && <h3>{ingredientsGroup.header}:</h3>}
 							<table>
-								{ingredientsGroup.ingredients.map(ingredient => (
-									<tr>
+								<tbody>
+								{ingredientsGroup.ingredients.map((ingredient, i) => (
+									<tr key={i}>
 										<td>
 											<span className="amount">{ingredient.amount}</span>
 											<span className="unit">{ingredient.unit}</span>
 										</td>
 										<td>
-											<span className="ingredientName">{ ingredient.name }</span>
-											{ ingredient.other && <span className="other">, { ingredient.other }</span> }
+											<span className="ingredientName">{ingredient.name}</span>
+											{ingredient.usageInfo &&
+											<span className="other">{ingredient.usageInfo}</span>}
 										</td>
 									</tr>
 								))}
+								</tbody>
 							</table>
 						</div>
 					))
 				}
 			</div>
-			<div className="description">
-				<h2>Instructions</h2>
+			<div
+				className="description"
+				/* TODO replace that with safer solution */
+				dangerouslySetInnerHTML={{ __html: recipeData?.instructions.replace(/(.*\n)/g, "<p>$1</p>")}}
+			>
+				{/*<h2>Instructions</h2>*/}
 
-				<p>Die Pasta in reichlich Salzwasser bissfest kochen. Den Schinken in Würfel schneiden und in wenig
-					Butter anbraten.</p>
+				{/*<p>Die Pasta in reichlich Salzwasser bissfest kochen. Den Schinken in Würfel schneiden und in wenig*/}
+				{/*	Butter anbraten.</p>*/}
 
-				<p>Eigelb in einer großen Schüssel mit Salz, Pfeffer und Muskat verquirlen. Die Butter schaumig
-					rühren und gut unter das Eigelb mischen. Die Schinkenwürfel und den geriebenen Käse gründlich
-					unterrühren.</p>
+				{/*<p>Eigelb in einer großen Schüssel mit Salz, Pfeffer und Muskat verquirlen. Die Butter schaumig*/}
+				{/*	rühren und gut unter das Eigelb mischen. Die Schinkenwürfel und den geriebenen Käse gründlich*/}
+				{/*	unterrühren.</p>*/}
 
-				<p>Wenn die Nudeln gar sind, abgießen, sofort zu der Mischung in die Schüssel geben, nochmal alles
-					gründlich durchmischen, dann sogleich servieren.</p>
+				{/*<p>Wenn die Nudeln gar sind, abgießen, sofort zu der Mischung in die Schüssel geben, nochmal alles*/}
+				{/*	gründlich durchmischen, dann sogleich servieren.</p>*/}
 			</div>
 		</div>
 	)
