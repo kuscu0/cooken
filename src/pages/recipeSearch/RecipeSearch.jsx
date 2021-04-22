c77773b4b79c96f1cf2c992018c31a84fd5f9b96
ArthurHeitmann <art.tec.15@gmail.com>
2021-04-22 08:17:02 +0200

    added search filter ui

--- src/pages/recipeSearch/RecipeSearch.jsx
+++ src/pages/recipeSearch/RecipeSearch.jsx
@@ -9,10 +9,13 @@ import {getImgSrcFromRecipeData, serverAddress} from "../../utils/utils";
 export default function RecipeSearch() {
 	const [ searchQuery, setSearchQuery ] = useState("");
 	const [ recipeResults, setRecipeResults ] = useState([]);
+	const [ rating, setRating ] = useState(-1);
+	const [ maxTime, setMaxTime ] = useState(0);
+	const [ maxDifficulty, setMaxDifficulty ] = useState(0);
 
 	async function search() {
 		const query = new URLSearchParams({
-			query: searchQuery
+			title: searchQuery
 		});
 		const r = await fetch(`${serverAddress}/search?${query.toString()}`);
 		const tmpRecipes = await r.json();
@@ -42,6 +45,34 @@ export default function RecipeSearch() {
 						</div>
 					}
 				/>
+				<div className="ratingFilter">
+					{ Array(5).fill(0).map(
+						(_, i) => <button
+							className={"ratingStar" + (i <= rating ? " active" : "")}
+							onClick={() => setRating(i === rating ? -1 : i)}
+							key={i}
+						/>
+					)}
+				</div>
+				<div className="maxTime">
+					<span>Max Time (Minutes)</span>
+					{[15, 30, 45, 60, 90].map(time => (
+						<button
+							onClick={() => setMaxTime(maxTime === time ? 0 : time)}
+							className={maxTime === time ? "active" : ""}
+							key={time}
+						>{time}</button>
+					))}
+				</div>
+				<div className="difficulty">
+					{["Easy", "Medium", "Hard"].map((diff, i) => (
+						<button
+							onClick={() => setMaxDifficulty(maxDifficulty === i +1 ? 0 : i + 1)}
+							className={maxDifficulty === i +1 ? "active" : ""}
+							key={i}
+						>{diff}</button>
+					))}
+				</div>
 				<SimpleButton onClick={search} className="searchButton">Search</SimpleButton>
 			</div>
 			<div className="results">
