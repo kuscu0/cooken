ab3c3ba3148e9691b1233efa37154893096db878
ArthurHeitmann <art.tec.15@gmail.com>
2021-06-13 21:55:19 +0200

    improved commenting

--- src/pages/recipe/comment/Comment.jsx
+++ src/pages/recipe/comment/Comment.jsx
@@ -1,11 +1,21 @@
 import "./Comment.scss"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {authFetch} from "../../../utils/utils";
 
 export default function Comment(props) {
     const [text, setText] = useState(props.initialText);
+    const [hasBeenSaved, setHasBeenSaved] = useState(false);
+
+    useEffect(() => {
+        if (!hasBeenSaved && text === "")
+            textInput.focus();
+    }, []);
 
     async function saveText() {
+        if (!hasBeenSaved && text === "") {
+            deleteComment();
+            return;
+        }
         await authFetch("/users/recipeComment", true, {
             method: "PUT",
             body: new URLSearchParams([
@@ -14,6 +24,7 @@ export default function Comment(props) {
                 ["text", text]
             ]),
         });
+        setHasBeenSaved(true);
     }
 
     async function deleteComment() {
@@ -27,12 +38,19 @@ export default function Comment(props) {
         });
     }
 
+    let textInput;
     const initialTextLines = props.initialText.split("\n");
     return (
         <div className="recipeComment">
             <span className="marker"/>
             <div className="comment">
-                <div className="textInput" contentEditable="true" onInput={e => setText(e.currentTarget.innerText)} onBlur={saveText}>
+                <div
+                    className="textInput"
+                    contentEditable="true"
+                    onInput={e => setText(e.currentTarget.innerText)}
+                    onBlur={saveText}
+                    ref={(input) => { textInput = input }}
+                >
                     { initialTextLines.slice(1).reduce((curr, next) => curr.concat(<div>{next}</div>), [initialTextLines[0]]) }
                 </div>
                 <button className="deleteBtn" onClick={deleteComment}>
