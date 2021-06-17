1a13092e3dac9108a752ef872080bd9e169646bd
ArthurHeitmann <art.tec.15@gmail.com>
2021-06-17 18:53:07 +0200

    removed first mockup

--- first-mockup/js/loadSite.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById("search-bar");
-input.focus();
-
-[].forEach.call(document.querySelectorAll('li:not(.category)'), function (f) {
-    f.style.display = 'none';
-});
